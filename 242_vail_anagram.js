51d8877abbb7ac499eb2e3225b02c70a6d291003
pdokto <96900713+pdokto@users.noreply.github.com>
2025-05-01 16:48:34 +0200

    Create 242_vail_anagram.js

---
+++ 242_vail_anagram.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let sortedS= s.split("").sort().join()
+    let sortedT= t.split("").sort().join()
+
+    if (sortedS === sortedT) {
+        return true
+    } else {
+        return false
+    }
+};
